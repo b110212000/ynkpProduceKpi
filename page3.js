deb79f665ba42af3664aecfaa965efb66c4e5783
Willy <willy.hung@cmcct.com.tw>
2026-03-05 10:17:24 +0800

    update select people

--- page3.js
+++ page3.js
@@ -13,3 +13,23 @@ $('#openRuleBtn').click(function() {
     // 使用你記得的寫法：呼叫 .modal('show') 把視窗彈出來！
     $('#voteRuleModal').modal('show');
 });
+
+// =========================================
+// 練習生頭像 單選邏輯
+// =========================================
+$('.avatar-circle').click(function() {
+    
+    // 1. 清除所有人的選取狀態 (把 selected-avatar 這個 class 拔掉)
+    $('.avatar-circle').removeClass('selected-avatar');
+    
+    // 2. 只為當前被點擊的這個頭像加上選取狀態
+    $(this).addClass('selected-avatar');
+    
+    // 3. (進階實用) 抓取你選到了誰！
+    // 透過 .siblings() 找到旁邊的 div 文字，你就可以知道選到了誰
+    let selectedName = $(this).siblings('.text-white').text();
+    console.log("目前 PICK 的練習生是：" + selectedName);
+
+    // 你也可以順便把底部按鈕的文字動態換掉，讓使用者更有感！
+    $('.bottom-action-btn .fs-5').text('確認 PICK：' + selectedName);
+});
